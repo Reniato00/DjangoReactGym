51f89a3ad002932f819ae795a00f6d578b37d298
Renato <renyralo@hotmail.com>
2023-08-13 22:19:39 -0600

    third upload

--- venv/api/app/admin.py
+++ venv/api/app/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Client,Validity
+from .models import Client,Validity,ClientValidity,Place,PlaceCategoryPlace,CategoryPlace,Coach,PlaceCoach
 
 # Register your models here.
 admin.site.register(Client)
 admin.site.register(Validity)
+admin.site.register(ClientValidity)
+admin.site.register(Place)
+admin.site.register(PlaceCategoryPlace)
+admin.site.register(CategoryPlace)
+admin.site.register(Coach)
+admin.site.register(PlaceCoach)
 
